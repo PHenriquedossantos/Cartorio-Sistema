f691cd5c143188f875f497aa96ff15d7568f1e1a
antonylrds <antonylrds@gmail.com>
2023-05-16 18:47:16 -0300

    Script for adding elements to emoluments table

---
+++ read_from_xlsx.py
@@ -0,0 +1,33 @@
+import pandas
+from src.models.emolument_db import Emolument
+from src.database.dbconfig import session
+
+columns_association = {
+    'CODIGO ATO': 'codigo',
+    'EMOLUMENTOS': 'emolumentos',
+    'DESCRICAO': 'descricao',
+    'TIPO_SELO': 'tipo_selo',
+    'FERMOJU': 'fermoju',
+    'SELO': 'valor_selo',
+    'SUB_TOTAL': 'subtotal',
+    'FAADEP': 'faadep',
+    'FRMMP': 'frmmp',
+    'TOTAL': 'total'
+}
+
+
+excel_data = pandas.read_excel('./public/tabela.xlsx')
+excel_data = excel_data.rename(mapper=columns_association, axis='columns')
+
+
+with session:
+    for row in excel_data.iterrows():
+        row_data = row[1]
+
+        new_emolument = Emolument(**row_data)
+        session.add(new_emolument)
+
+    session.commit()
+
+
+
